e057fc4561c00e76a3e69e244b9b195145f97c6a
Elias Hania <eliasnumero1@gmail.com>
2024-07-08 20:55:47 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+import robotsTxt from "astro-robots-txt"
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: {
-    base: 'https://eliashania.github.io/portfolio/', // Reemplaza con la URL de tu GitHub Pages
-  },
-  output: 'static', // Asegúrate de que la salida sea estática
-});
+  site: 'https://eliashania.github.io/portfolio/'
+})
